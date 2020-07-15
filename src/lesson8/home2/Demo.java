e0a4d6c6f0c473a130c64c4a76f04f70336de583
Crabislav <alex.kopnin2@gmail.com>
2020-07-15 17:57:25 +0300

    createHighestParent and createLowestChild became static

--- src/lesson8/home2/Demo.java
+++ src/lesson8/home2/Demo.java
@@ -8,18 +8,18 @@ public class Demo {
 
         Demo demo = new Demo();
 
-        Student student = demo.createHighestParent("Alex", "Kopnin", 1, coursesTaken);
-        SpecialStudent specialStudent = demo.createLowestChild("Andrey", "Petrov", 2, coursesTaken, 5464, "example@mail.com");
+        Student student = createHighestParent("Alex", "Kopnin", 1, coursesTaken);
+        SpecialStudent specialStudent = createLowestChild("Andrey", "Petrov", 2, coursesTaken, 5464, "example@mail.com");
 
         System.out.println("createHighestParent result : " + student.firstName + " " + student.lastName);
         System.out.println("createLowestChild result : " + specialStudent.firstName + " " + specialStudent.lastName);
     }
 
-    public Student createHighestParent(String firstName, String lastName, int group, Course[] coursesTaken) {
+    public static Student createHighestParent(String firstName, String lastName, int group, Course[] coursesTaken) {
         return new Student(firstName, lastName, group, coursesTaken);
     }
 
-    public SpecialStudent createLowestChild(String firstName, String lastName, int group, Course[] coursesTaken,
+    public static SpecialStudent createLowestChild(String firstName, String lastName, int group, Course[] coursesTaken,
                                             long secretKey, String email) {
         return new SpecialStudent(firstName, lastName, group, coursesTaken, secretKey, email);
     }
