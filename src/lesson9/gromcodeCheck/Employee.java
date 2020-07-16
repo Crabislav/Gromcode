14f2057c09ca5eaaa329ff77fbee16864e667ab8
Crabislav <alex.kopnin2@gmail.com>
2020-07-16 15:48:08 +0300

    add duplicates of Company.java and Employee.java

---
+++ src/lesson9/gromcodeCheck/Employee.java
@@ -0,0 +1,16 @@
+package lesson9.gromcodeCheck;
+
+import lesson9.Company;
+
+public class Employee {
+    String name;
+    Company company;
+    int monthlySalary;
+
+
+    public Employee(String name, Company company, int monthlySalary) {
+        this.name = name;
+        this.company = company;
+        this.monthlySalary = monthlySalary;
+    }
+}
