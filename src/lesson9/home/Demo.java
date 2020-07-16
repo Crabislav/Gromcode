c205d1d7cdc509cbf79562c158a0fca6e087827c
Crabislav <alex.kopnin2@gmail.com>
2020-07-16 21:20:15 +0300

    solution for homework for lesson9 was added

---
+++ src/lesson9/home/Demo.java
@@ -0,0 +1,88 @@
+package lesson9.home;
+
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+
+
+        UserRepository userRepository = new UserRepository(generateUsers(5));
+
+        User[] users = new User[5];
+        UserRepository userRepository1 = new UserRepository(users);
+
+        System.out.println("Part 2");
+        checkHomeworkPart2(userRepository);
+        checkHomeworkPart2(userRepository1);
+
+        System.out.println("\nPart 3");
+        checkHomeworkPart3(userRepository);
+        checkHomeworkPart3(userRepository1);
+
+        System.out.println("\nPart 4");
+        checkHomeworkPart4();
+
+        System.out.println("\nPart 5");
+        User[] users1 = new User[2];
+        UserRepository userRepository2 = new UserRepository(users1);
+        checkHomeworkPart5(userRepository2);
+
+    }
+
+    public static User[] generateUsers(int count) {
+        if (count > 0) {
+            User[] users = new User[count];
+            for (int i = 0; i <= users.length - 1; i++) {
+                users[i] = new User(i, "Name" + i, "Session" + i);
+            }
+            return users;
+        }
+        return null;
+    }
+
+    public static void checkHomeworkPart2(UserRepository userRepository) {
+        //getUserNames test
+        String[] userNames = userRepository.getUserNames();
+        System.out.println(Arrays.toString(userNames));
+
+        //getUserIds test
+        long[] ids = userRepository.getUserIds();
+        System.out.println(Arrays.toString(ids));
+
+        //getUserNameById test
+        System.out.println(userRepository.getUserNameById(1));
+        System.out.println(userRepository.getUserNameById(-1));
+    }
+
+    public static void checkHomeworkPart3(UserRepository userRepository) {
+        //getUserByName test
+        System.out.println(userRepository.getUserByName("Name1"));
+        System.out.println(userRepository.getUserByName("Name")); //null
+
+        //getUserNameById became private
+//        System.out.println("\n" + userRepository.findUserById(1));
+//        System.out.println(userRepository.findUserById(-1));  //null
+
+        System.out.println(userRepository.getUserBySessionId("Session1"));
+        System.out.println(userRepository.getUserBySessionId("session") + "\n");  //null
+    }
+
+    public static void checkHomeworkPart4() {
+        User[] users = new User[1];
+        UserRepository userRepository = new UserRepository(users);
+
+        User user = new User(8, "Name8", "Session8");
+        System.out.println(userRepository.save(user));
+
+        User user1 = new User(8, "Name7", "Session8");
+        System.out.println(userRepository.save(user1) + "\n");
+
+    }
+
+    public static void checkHomeworkPart5(UserRepository userRepository) {
+        User user = new User(9, "Name9", "Session9");
+        System.out.println(userRepository.save(user));
+        userRepository.delete(9);
+        System.out.println(userRepository.toString());
+    }
+}
---
+++ src/lesson9/home/UserRepository.java
@@ -0,0 +1,143 @@
+package lesson9.home;
+
+import java.util.Arrays;
+
+public class UserRepository {
+    private User[] users;
+
+
+    public UserRepository(User[] users) {
+        this.users = users;
+    }
+
+    public User[] getUsers() {
+        return users;
+    }
+
+    //Homework.part2
+    public String[] getUserNames() {
+        if (users == null)
+            return null;
+
+        int index = 0;
+        String[] userNames = new String[users.length];
+
+        for (User user : users) {
+            if (user != null) {
+                userNames[index] = user.getName();
+                index++;
+            }
+        }
+        return userNames;
+    }
+
+    public long[] getUserIds() {
+        if (users == null) {
+            return null;
+        }
+
+        int index = 0;
+        long[] ids = new long[users.length];
+
+        for (User user : users) {
+            if (user != null) {
+                ids[index] = user.getId();
+                index++;
+            }
+        }
+        return ids;
+    }
+
+    public String getUserNameById(long id) {
+        if (users != null) {
+            for (User user : users) {
+                if (user != null && user.getId() == id)
+                    return user.getName();
+            }
+        }
+        return null;
+    }
+
+    //Homework.part3
+    public User getUserByName(String name) {
+        if (users != null) {
+            for (User user : users) {
+                if (user != null && user.getName().equals(name)) {
+                    return user;
+                }
+            }
+        }
+        return null;
+    }
+
+    //renamed from getUserById to findUserById + public -> private
+    private User findUserById(long id) {
+        if (users != null) {
+            for (User user : users) {
+                if (user != null && user.getId() == id) {
+                    return user;
+                }
+            }
+        }
+        return null;
+    }
+
+    public User getUserBySessionId(String sessionId) {
+        if (users != null) {
+            for (User user : users) {
+                if (user != null && user.getSessionId().equals(sessionId)) {
+                    return user;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    //Homework.part4
+    public User save(User user) {
+        if (user != null && users != null && findUserById(user.getId()) == null) {
+            for (int i = 0; i < users.length; i++) {
+                if (users[i] == null) {
+                    users[i] = user;
+                    return users[i];
+                }
+            }
+        }
+        return null;
+    }
+
+
+    //Homework.part5
+    public User update(User user) {
+        if (user != null && users != null && findUserById(user.getId()) == null) {
+            for (int i = 0; i < users.length; i++) {
+                if (users[i].getId() == user.getId()) {
+                    users[i] = user;
+                    return users[i];
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public void delete(long id) {
+        if (users == null)
+            return;
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].equals(findUserById(id))) {
+                users[i] = null;
+                return;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "UserRepository{" +
+                "users=" + Arrays.toString(users) +
+                '}';
+    }
+}
+
