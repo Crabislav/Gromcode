8ee26d1ae1ffd3e082c3930eeae77853291f215e
Crabislav <alex.kopnin2@gmail.com>
2020-07-18 19:56:23 +0300

    add lesson's examples

---
+++ src/lesson10/abstractfirstexample/PostgreSQLDbConnector.java
@@ -0,0 +1,14 @@
+package lesson10.abstractfirstexample;
+
+public class PostgreSQLDbConnector extends DbProvider {
+
+    @Override
+    void connectToDb() {
+        //logic for Postgres
+    }
+
+    @Override
+    void disconnectFromDb() {
+        //logic Postgres
+    }
+}
