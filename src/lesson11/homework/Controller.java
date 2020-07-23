3abdbf33ac65521e152cae6ccf52401e7bd29b24
Crabislav <alex.kopnin2@gmail.com>
2020-07-23 21:04:16 +0300

    add requestRooms method

---
+++ src/lesson11/homework/Controller.java
@@ -0,0 +1,56 @@
+package lesson11.homework;
+
+public class Controller {
+    private API[] apis;
+
+    public Controller(API[] apis) {
+        this.apis = apis;
+    }
+
+    //TODO: finish up requestRooms method
+    //finds rooms among all APIs
+    public Room[] requestRooms(int price, int persons, String city, String hotel) {
+        int amount = calculateRoomsAmount(price, persons, city, hotel);
+
+        if (amount <= 0) {
+            return null;
+        }
+
+        Room[] resultRooms = new Room[amount];
+
+        int index = 0;
+        for (API api : apis) {
+            Room[] rooms = api.findRooms(price, persons, city, hotel);
+            for (Room room : rooms) {
+                resultRooms[index] = room;
+                index++;
+            }
+        }
+        return resultRooms;
+    }
+
+    /*//TODO: finish up check method
+    //finds similar rooms among two APIs and adds them to the first API's array
+    public Room[] check(API api1, API api2) {
+        return;
+    }
+
+    //TODO: finish up cheapestRoom method
+    //find the cheapest room among all APIs
+    public Room cheapestRoom() {
+        for (API api : apis) {
+
+        }
+    }*/
+
+    private int calculateRoomsAmount(int price, int persons, String city, String hotel) {
+        int count = 0;
+        for (API api : apis) {
+            if (api != null) {
+                count += api.findRooms(price, persons, city, hotel).length;
+            }
+        }
+        return count;
+    }
+}
+
--- src/lesson11/homework/DemoHomework.java
+++ src/lesson11/homework/DemoHomework.java
@@ -8,49 +8,43 @@ public class DemoHomework {
         Room[] emptyRoomArray = new Room[5];
 
         //GoogleAPI Test
-        Room room1 = new Room(1, 150, 2, new Date(), "Hotel", "City");
-        Room room2 = new Room(2, 100, 2, new Date(), "Hotel", "City");
-        Room room3 = new Room(3, 150, 2, new Date(), "Hotsadel", "City");
-        Room room4 = new Room(4, 100, 2, new Date(), "Hotel", "Citdy");
-        Room room5 = new Room(5, 100, 2, new Date(), "Hotel", "City");
+        Room room1 = new Room(881, 100, 9, new Date(), "Hotel", "City");
+        Room room2 = new Room(222, 100, 2, new Date(), "Hotel", "City");
+        Room room3 = new Room(311, 100, 2, new Date(), "Hotel", "City");
+        Room room4 = new Room(474, 100, 2, new Date(), "Hotel", "Hotel");
+        Room room5 = new Room(5357, 1900, 2, new Date(), "Hotel", "City");
 
-        Room[] rooms = new Room[]{room1, room2, room3, room4, room5};
-        GoogleAPI googleAPI = new GoogleAPI(rooms);
+        Room[] googleAPIRooms = new Room[]{room1, room2, room3, room4, room5};
 
-        printDivider();
-        System.out.println("GoogleAPI Test");
-        printDivider();
+        GoogleAPI googleAPI = new GoogleAPI(googleAPIRooms);
+        GoogleAPI emptyGoogleAPI = new GoogleAPI(emptyRoomArray);
 
-        System.out.println(Arrays.toString(googleAPI.findRooms(100, 2, "City", "Hotel")));
-        System.out.println("\n");
 
-        GoogleAPI googleAPI1 = new GoogleAPI(emptyRoomArray);
-        System.out.println(Arrays.toString(googleAPI1.findRooms(100, 2, "City", "Hotel")));
-        System.out.println("\n");
-
-        //TripAdvisor Test
         Room room11 = new Room(1, 100, 6, new Date(), "Hotel", "City");
-        Room room22 = new Room(2, 100, 2, new Date(), "Hotel", "City");
-        Room room33 = new Room(3, 100, 2, new Date(), "Hotel", "City");
-        Room room44 = new Room(4, 100, 3, new Date(), "Hotel", "City");
-        Room room55 = new Room(5, 100, 4, new Date(), "Hotel", "City");
+        Room room22 = new Room(45, 100, 2, new Date(), "Hotel", "City");
+        Room room33 = new Room(553, 100, 2, new Date(), "Hotel", "City");
+        Room room44 = new Room(664, 100, 3, new Date(), "Hotel", "City");
+        Room room55 = new Room(85, 100, 4, new Date(), "Hotel", "City");
 
+        Room[] TripAdvisorAPIRooms = new Room[]{room11, room22, room33, room44, room55};
 
-        Room[] rooms1 = new Room[]{room11, room22, room33, room44, room55};
-        TripAdvisorAPI tripAdvisorAPI = new TripAdvisorAPI(rooms1);
+        TripAdvisorAPI tripAdvisorAPI = new TripAdvisorAPI(TripAdvisorAPIRooms);
+        TripAdvisorAPI emptyTripAdvisorAPI = new TripAdvisorAPI(emptyRoomArray);
+
 
         printDivider();
-        System.out.println("TripAdvisor Test");
+        System.out.println("API Test");
         printDivider();
-        System.out.println(Arrays.toString(tripAdvisorAPI.findRooms(100, 2, "City", "Hotel")));
-        System.out.println("\n");
 
-
-        TripAdvisorAPI tripAdvisorAPI1 = new TripAdvisorAPI(emptyRoomArray);
-        System.out.println(Arrays.toString(tripAdvisorAPI1.findRooms(100, 2, "City", "Hotel")));
-        System.out.println("\n");
+        API[] apis = new API[]{googleAPI, emptyTripAdvisorAPI, tripAdvisorAPI};
+        Controller controller = new Controller(apis);
+        for (API api : apis) {
+            System.out.println("\n" + api.getClass());
+            System.out.println(Arrays.toString(controller.requestRooms(100, 2, "City", "Hotel")));
+        }
 
     }
+
 
     private static void printDivider() {
         for (int i = 0; i < 50; i++) {
@@ -58,6 +52,5 @@ public class DemoHomework {
         }
         System.out.println();
     }
-
 
 }
