95e895d8b39e9e0c538249c4b2e8c41bc5a9a219
Crabislav <alex.kopnin2@gmail.com>
2020-07-28 02:11:14 +0300

    add UkrainianBankSystem class

---
+++ src/lesson12/UkrainianBankSystem.java
@@ -0,0 +1,94 @@
+package lesson12;
+
+public class UkrainianBankSystem implements BankSystem {
+
+    @Override
+    public void withdraw(User user, int amount) {
+        if (!checkWithdraw(user, amount)) {
+            return;
+        }
+        setBalanceAfterWithdraw(user, amount);
+    }
+
+
+    @Override
+    public void fund(User user, int amount) {
+        if (!checkFund(user, amount)) {
+            return;
+        }
+        setBalanceAfterFund(user, amount);
+    }
+
+
+    @Override
+    public void transferMoney(User fromUser, User toUser, int amount) {
+        //withdraw money from User
+        //fund money to User
+
+        if (!checkWithdraw(fromUser, amount) && !checkFund(toUser, amount)) {
+            printTransferErrorMsg(amount, fromUser, toUser);
+            return;
+        }
+
+        setBalanceAfterWithdraw(fromUser, amount);
+        setBalanceAfterFund(toUser, amount);
+    }
+
+    @Override
+    public void paySalary(User user) {
+        fund(user, user.getSalary());
+    }
+
+
+    private void printWithdrawalErrorMsg(int amount, User user) {
+        System.err.println("Can't withdraw money " + amount + "\n   from " + user.toString());
+    }
+
+    private void printFundErrorMsg(int amount, User user) {
+        System.err.println("Can't fund money " + amount + "\n   from " + user.toString());
+    }
+
+    private void printTransferErrorMsg(int amount, User fromUser, User toUser) {
+        System.err.println("Can't transfer money " + amount + "\n   from" + fromUser.toString() + "\n   to " + toUser.toString());
+    }
+
+
+    private boolean checkWithdraw(User user, int amount) {
+        return checkWithdrawLimits(user, amount, user.getBank().getLimitOfWithdrawal()) &&
+                checkWithdrawLimits(user, amount, user.getBalance());
+    }
+
+    private boolean checkFund(User user, int amount) {
+        if (amount + user.getBank().getCommission(amount) > user.getBank().getLimitOfFunding() || amount <= 0) {
+            printFundErrorMsg(amount, user);
+            return false;
+        }
+        return true;
+    }
+
+    //I'd added check for negative withdraw amount here
+    private boolean checkWithdrawLimits(User user, int amount, double limit) {
+        if (amount + user.getBank().getCommission(amount) > limit || amount <= 0) {
+            printWithdrawalErrorMsg(amount, user);
+            return false;
+        }
+        return true;
+    }
+
+    private void setBalanceAfterWithdraw(User user, int amount) {
+        //code bellow is according to the video-lesson
+//        user.setBalance(user.getBalance() - amount - amount * user.getBank().getCommission(amount));
+
+        //code after using math rule : a*b - a*c = a*(b - c)
+        user.setBalance(user.getBalance() - amount * (1 - user.getBank().getCommission(amount)));
+    }
+
+    private void setBalanceAfterFund(User user, int amount) {
+        //code bellow is according to the video-lesson
+//        user.setBalance(user.getBalance() + amount - amount * user.getBank().getCommission(amount));
+
+        //code after using math rule : a*b - a*c = a*(b - c)
+        user.setBalance(user.getBalance() + amount * (1 - user.getBank().getCommission(amount)));
+    }
+
+}
