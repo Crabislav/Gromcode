61479531e15d4670d65c009f0f816ee88e369828
Crabislav <alex.kopnin2@gmail.com>
2020-08-05 00:27:29 +0300

    added simple tests

--- src/lesson15/homework/Demo.java
+++ src/lesson15/homework/Demo.java
@@ -1,40 +1,112 @@
 package lesson15.homework;
 
-import java.util.Arrays;
 
 public class Demo {
     public static void main(String[] args) {
-        //creating userRepository
-        UserRepository userRepository = new UserRepository(generateUsers(2, 1, "Alex"));
-//        System.out.println(userRepository.toString());
+        //fully filled array
+        UserRepository userRepository = new UserRepository(generateUsers(5));
 
-        //findUser test
-        User userToFind = new User(1, "Alex", "session1");
-//        System.out.println(userRepository.findUser(userToFind).toString());
+        //empty array
+        UserRepository emptyUserRepository = new UserRepository(new User[5]);
+
+        User user1 = new User(8, "Name8", "Session8");
+        User user2 = new User(8, "sadsd", "asdasdasd");
 
         //save test
-        User userToSave = new User(8, "Andrew", "asdsad");
-        userRepository.save(userToSave);
-        User userToSave1 = new User(8, "Andrew", "asdsad");
-        System.out.println(userRepository.save(userToSave1));
+        System.out.println("save test");
+        //fully filled array +
+        System.out.println("Fully filled array");
+        //saving a user when there is no space for him +
+        System.out.println(userRepository.toString());
+        userRepository.save(user1);
+        System.out.println(userRepository.toString());
+        //null input
+        userRepository.save(null);
+        System.out.println(userRepository.toString() + "\n");
+
+        //empty array +
+        System.out.println("Empty array");
+        //saving a user +
+        System.out.println(emptyUserRepository.toString());
+        emptyUserRepository.save(user1);
+        System.out.println(emptyUserRepository.toString() + "\n");
+
+        //saving the user with the same id +
+        System.out.println(emptyUserRepository.toString());
+        emptyUserRepository.save(user2);
+        System.out.println(emptyUserRepository.toString() + "\n");
+
+        //null input +
+        System.out.println(emptyUserRepository.toString());
+        emptyUserRepository.save(null);
+        System.out.println(emptyUserRepository.toString() + "\n");
+
+        //delete test
+        System.out.println("delete test");
+        //fully filled array +
+        System.out.println("Fully filled array");
         System.out.println(userRepository.toString());
 
+        //delete using correct id +
+        userRepository.delete(1);
+        System.out.println(userRepository.toString() + "\n");
+        //delete using wrong id +
+        userRepository.delete(1);
+        System.out.println(userRepository.toString() + "\n");
 
+        //empty array +
+        System.out.println("Empty array");
+        System.out.println(emptyUserRepository.toString() + "\n");
+        //delete using correct id +
+        emptyUserRepository.delete(8);
+        System.out.println(emptyUserRepository.toString() + "\n");
+        //delete using wrong id +
+        emptyUserRepository.delete(8);
+        System.out.println(emptyUserRepository.toString() + "\n");
+
+
+        //update test
+        System.out.println("Update test");
+        //fully filled array
+        System.out.println("Fully filled array");
+        //update using correct user id +
+        System.out.println(userRepository.toString());
+        User user3 = new User(0, "Alex", "asdasda");
+        userRepository.update(user3);
+        System.out.println(userRepository.toString() + "\n");
+
+        //update using wrong user id  +
+        System.out.println(userRepository.toString());
+        user3 = new User(545, "Alex", "asdasda");
+        userRepository.update(user3);
+        System.out.println(userRepository.toString() + "\n");
+
+        //null input +
+        System.out.println(userRepository.toString());
+        userRepository.update(null);
+        System.out.println(userRepository.toString() + "\n");
+
+        //empty array +
+        System.out.println("Empty array");
+        //update any user id +
+        System.out.println(emptyUserRepository.toString());
+        emptyUserRepository.update(user3);
+        System.out.println(emptyUserRepository.toString() + "\n");
+
+        //null input
+        System.out.println(emptyUserRepository.toString());
+        emptyUserRepository.update(null);
+        System.out.println(emptyUserRepository.toString() + "\n");
     }
 
-    private static User[] generateUsers(int amountToGenerate, int startId, String name) {
-        if (amountToGenerate <= 0 || name == null || startId <= 0) {
-            return new User[0];
+    public static User[] generateUsers(int count) {
+        if (count > 0) {
+            User[] users = new User[count];
+            for (int i = 0; i <= users.length - 1; i++) {
+                users[i] = new User(i, "Name" + i, "Session" + i);
+            }
+            return users;
         }
-        int emptyArraySlotsAmount = 5;
-        User[] users = new User[amountToGenerate + emptyArraySlotsAmount];
-
-        for (int i = 0; i < users.length - emptyArraySlotsAmount; i++) {
-            users[i] = new User(startId, name, "session" + startId);
-            startId++;
-        }
-        return users;
-
-
+        return new User[0];
     }
 }
