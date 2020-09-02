d3d3c0e1606015d7d465baec5e8946a133fc2a92
Crabislav <alex.kopnin2@gmail.com>
2020-09-02 23:17:23 +0300

    added exercise2

---
+++ src/lesson18/exercise2/Converter.java
@@ -0,0 +1,31 @@
+package lesson18.exercise2;
+
+public class Converter {
+
+    public static void main(String[] args) {
+        System.out.println(intToString(111231));
+        System.out.println(StringToInt("11"));
+
+        System.out.println(StringToInt("1050a"));
+
+        System.out.println(intToString(111231));
+        System.out.println(StringToInt("11"));
+
+        System.out.println(intToString(111231));
+        System.out.println(StringToInt("11"));
+
+    }
+
+    public static String intToString(int input) {
+        return String.valueOf(input);
+    }
+
+    public static int StringToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (Exception e) {
+            System.out.println(input + " cannot be converted to int");
+            return 0;
+        }
+    }
+}
