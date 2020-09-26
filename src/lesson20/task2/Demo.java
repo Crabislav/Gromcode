1dd27c76ac1188aba5254e5051eed6d988033093
Crabislav <alex.kopnin2@gmail.com>
2020-09-26 16:41:24 +0300

    changed and added new test values

--- src/lesson20/task2/Demo.java
+++ src/lesson20/task2/Demo.java
@@ -9,22 +9,34 @@ public class Demo {
 
         Transaction transaction1 = new Transaction(0, "Kiev", 10, "Money", TransactionType.INCOME, todayDate);
         Transaction transaction2 = new Transaction(1, "Odessa", 10, "Paper", TransactionType.OUTCOME, todayDate);
-        Transaction transaction3 = new Transaction(2, "Zaporoje", 20, "MOOONEY", TransactionType.INCOME, todayDate);
-        Transaction transaction4 = new Transaction(3, "Odessa", 20, "Paper", TransactionType.OUTCOME, todayDate);
+        Transaction transactionInvalidCity = new Transaction(2, "Zaporoje", 20, "MOOONEY", TransactionType.INCOME, todayDate);
+        Transaction transaction4 = new Transaction(3, "Odessa", 10, "Paper", TransactionType.OUTCOME, todayDate);
+        Transaction transaction5 = new Transaction(4, "Odessa", 10, "Paper", TransactionType.OUTCOME, todayDate);
+        Transaction transaction6 = new Transaction(5, "Odessa", 10, "Paper", TransactionType.OUTCOME, todayDate);
+        Transaction transaction7 = new Transaction(6, "Odessa", 5, "Paper", TransactionType.OUTCOME, todayDate);
 
-        Transaction[] transactions = {transaction1, transaction2, transaction3, transaction4};
+        Transaction[] trNormal = {transaction1, transaction2, transaction4, transaction5, transaction6};
+        Transaction[] trInvalidCity = {transaction1, transaction2, transactionInvalidCity, transaction4, transaction5, transaction6};
+        Transaction[] trNoSpace = {transaction1, transaction2, transaction4, transaction5, transaction6, transaction7};
+        Transaction[] trInvalidAllAmount = {transaction1, transaction2, transaction4, transaction5, transaction6, transaction7};
+
+//        testMethods(trNormal, "Kiev", 10);
+//        testMethods(trInvalidCity, "Kiev", 10);
+//        testMethods(trNoSpace, "Kiev", 10);
+        testMethods(trInvalidAllAmount, "Kiev", 10);
+    }
+
+    private static void testMethods(Transaction[] trNormal, String city, int amount) {
         Controller controller = new Controller();
 
-
         //save test
-        for (Transaction element : transactions) {
+        for (Transaction element : trNormal) {
             try {
                 controller.save(element);
             } catch (Exception e) {
                 System.err.println(e.getMessage());
             }
         }
-
 
         //transactionList - empty
         try {
@@ -38,7 +50,7 @@ public class Demo {
         //transactionList - amount
         try {
             System.out.println("\nTransaction list. Amount filter");
-            System.out.println(Arrays.toString(controller.transactionList(10)));
+            System.out.println(Arrays.toString(controller.transactionList(amount)));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -46,7 +58,7 @@ public class Demo {
         //transactionList - city
         try {
             System.out.println("\nTransaction list. City filter");
-            System.out.println(Arrays.toString(controller.transactionList("Kiev")));
+            System.out.println(Arrays.toString(controller.transactionList(city)));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
