91360d0781069f63fa27e940c7f8c9bc2b74fb30
Crabislav <alex.kopnin2@gmail.com>
2020-10-04 01:54:11 +0300

    added lesson27.homework2

---
+++ src/lesson27/homework2/Demo.java
@@ -0,0 +1,106 @@
+package lesson27.homework2;
+
+import java.util.ArrayList;
+
+public class Demo {
+    public static void main(String[] args) {
+        testMethods();
+    }
+
+    private static void testMethods() {
+        ArrayList<User> users = new ArrayList<>();
+        UserRepository userRepository = new UserRepository(users);
+
+        testSaveMethod(userRepository);
+        System.out.println();
+
+        testUpdateMethod(userRepository);
+        System.out.println();
+
+        testFindByIdMethod(userRepository);
+        System.out.println();
+
+        testDeleteMethod(userRepository);
+    }
+
+    private static void testUpdateMethod(UserRepository userRepository) {
+        System.out.println("Before testUpdateMethod\n" + userRepository.getUsers());
+        //update an existing user +
+        //update an invalid user+
+
+        User userToUpdate = new User(6, "Alex", "session885");
+        User invalidUser = new User(8, "name", "session");
+
+        User[] users = {userToUpdate, invalidUser};
+
+        for (User user : users) {
+            try {
+                userRepository.update(user);
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        System.out.println("After testUpdateMethod\n" + userRepository.getUsers());
+    }
+
+    private static void testFindByIdMethod(UserRepository userRepository) {
+        System.out.println("testFindByIdMethod");
+
+        //existing id+
+        //invalid id +
+
+        int[] ids = {6, 9};
+        for (int id : ids) {
+            try {
+                System.out.println(userRepository.findById(id));
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void testDeleteMethod(UserRepository userRepository) {
+        System.out.println("Before testDeleteMethod");
+
+        //existing id+
+        //invalid id +
+
+        int[] ids = {6, 9};
+        for (int id : ids) {
+            try {
+                System.out.println(userRepository.findById(id));
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        System.out.println("After testDeleteMethod");
+    }
+
+    private static void testSaveMethod(UserRepository userRepository) {
+        System.out.println("Before testSaveMethod\n" + userRepository.getUsers());
+        User user = new User(6, "name6", "session6");
+
+        //normal saving+
+        try {
+            userRepository.save(user);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        //saving the same user +
+        try {
+            userRepository.save(user);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        //saving null
+        try {
+            userRepository.save(null);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        System.out.println("After testSaveMethod\n" + userRepository.getUsers());
+    }
+}
---
+++ src/lesson27/homework2/UserRepository.java
@@ -0,0 +1,75 @@
+package lesson27.homework2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class UserRepository {
+    private ArrayList<User> users;
+
+    public UserRepository(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public User findById(long id) throws Exception {
+        for (User user : users) {
+            if (user != null && user.getId() == id) {
+                return user;
+            }
+        }
+        throw new Exception("findById : User with id=" + id + " wasn't found");
+    }
+
+    public User save(User user) throws Exception {
+        if (user == null) {
+            throw new Exception("save : Can't save null user");
+        }
+
+        if (users.contains(user)) {
+            throw new Exception("save : User (id =" + user.getId() + ") is already exists. Can't save this user again.");
+        }
+
+        users.add(user);
+        return user;
+    }
+
+    public User update(User user) throws Exception {
+        if (user == null) {
+            throw new Exception("update : Can't update null user");
+        }
+
+        if (!users.contains(user)) {
+            throw new Exception("update : User (id=" + user.getId() + ") doesn't exist. Can't do update");
+        }
+
+        int index = 0;
+        for (User element : users) {
+            if (element.equals(user)) {
+                users.set(index, user);
+                return user;
+            }
+            index++;
+        }
+        throw new Exception("update : Unexpected error");
+    }
+
+    public void delete(long id) throws Exception {
+        for (User user : users) {
+            if (user != null && user.getId() == id) {
+                users.remove(findById(id));
+                return;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "UserRepository{" +
+                "users=" + users +
+                '}';
+    }
+}
+
