7dd1c6b6a56acdbf8eff52a34fddadbaba50a733
Crabislav <alex.kopnin2@gmail.com>
2020-10-06 15:41:22 +0300

    code refactor

--- src/lesson28/comparatorexample/comparators/DateComparator.java
+++ src/lesson28/comparatorexample/comparators/DateComparator.java
@@ -3,22 +3,10 @@ package lesson28.comparatorexample.comparators;
 import lesson28.comparatorexample.Capability;
 
 import java.util.Comparator;
-import java.util.Date;
 
 public class DateComparator implements Comparator<Capability> {
     @Override
     public int compare(Capability o1, Capability o2) {
-        Date o1DateCreated = o1.getDateCreated();
-        Date o2DateCreated = o2.getDateCreated();
-
-        if (o1DateCreated == null && o2DateCreated == null) {
-            return 0;
-        } else if (o2DateCreated == null) {
-            return -1;
-        } else if (o1DateCreated == null) {
-            return 1;
-        } else {
-            return o2DateCreated.compareTo(o1DateCreated);
-        }
+        return ComparatorUtils.compareFields(o2.getDateCreated(), o1.getDateCreated());
     }
 }
