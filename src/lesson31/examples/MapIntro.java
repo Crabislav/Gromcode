b7b066af73294ea3388b266cf4588d5bd338ddb4
Crabislav <alex.kopnin2@gmail.com>
2020-10-14 05:29:37 +0300

    add lesson31.examples

---
+++ src/lesson31/examples/MapIntro.java
@@ -0,0 +1,31 @@
+package lesson31.examples;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapIntro {
+    public static void main(String[] args) {
+        Map<String, Building> map = new HashMap<>();
+
+        Building building = new Building("school", 5);
+        Building building1 = new Building("house", 9);
+
+        map.put("21tadd", building);
+        map.put("asdasd", building1);
+        System.out.println(map);
+
+        System.out.println(map.keySet());
+
+        System.out.println(map.values());
+
+        System.out.println(map.get("asdassss"));
+
+        map.remove("21tadd");
+
+        System.out.println(map);
+
+//        map.put(null, building);
+
+        map.put("asdasd", building1);
+    }
+}
