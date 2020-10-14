9c1f810676f99c19330b94437ec4d56568772e94
Crabislav <alex.kopnin2@gmail.com>
2020-10-14 06:16:32 +0300

    code refactor

--- src/lesson31/homework/Solution.java
+++ src/lesson31/homework/Solution.java
@@ -5,44 +5,52 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-    // Словом считаем набор букв, длинной больше 2
-
     //возвращать Map, которая хранит количество повторей каждого буквенного символа в этом тексте
     static Map<Character, Integer> countSymbols(String text) throws Exception {
         validateInput(text);
 
         Map<Character, Integer> result = new HashMap<>();
 
-        char[] chars = text.trim().toCharArray();
+        Character[] chars = text.chars()
+                .mapToObj(ch -> (char) ch)
+                .toArray(Character[]::new);
 
-        for (char aChar : chars) {
+        for (Character aChar : chars) {
             if (!Character.isLetter(aChar) || result.containsKey(aChar)) {
                 continue;
             }
-
-            int count = 0;
-
-            for (char ch : chars) {
-                if (aChar == ch) {
-                    count++;
-                }
-            }
-            result.put(aChar, count);
+            result.put(aChar, getRepeats(chars, aChar));
         }
         return result;
     }
 
-
-    // Второй метод words, должен возвращать Map, которая содержить ко-во повторений слов во входящем тексте.
-
-    Map<String, Integer> words(String text) throws Exception {
+    // Второй метод words, должен возвращать Map, которая содержит ко-во повторений слов во входящем тексте.
+    // Словом считаем набор букв, длинной больше 2
+    static Map<String, Integer> words(String text) throws Exception {
         validateInput(text);
-
 
         Map<String, Integer> result = new HashMap<>();
 
+        String[] words = text.split(" ");
 
+        for (String word : words) {
+            if (!isWord(word) || result.containsKey(word)) {
+                continue;
+            }
+            result.put(word, getRepeats(words, word));
+        }
         return result;
+    }
+
+    private static <T> int getRepeats(T[] array, T element) {
+        int count = 0;
+
+        for (T t : array) {
+            if (t.equals(element)) {
+                count++;
+            }
+        }
+        return count;
     }
 
     private static void validateInput(String text) throws Exception {
@@ -55,5 +63,7 @@ public class Solution {
         }
     }
 
-
+    private static boolean isWord(String word) {
+        return word.length() > 2;
+    }
 }
