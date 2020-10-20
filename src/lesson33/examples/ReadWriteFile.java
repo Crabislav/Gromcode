41c743bb9b664146377e5671e01a7ed164ab792a
Crabislav <alex.kopnin2@gmail.com>
2020-10-20 23:06:00 +0300

    code refactor

--- src/lesson33/examples/ReadWriteFile.java
+++ src/lesson33/examples/ReadWriteFile.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class ReadWriteFile {
     public static void main(String[] args) {
         readFile("C:/Users/Alex Kopnin/Desktop/lib/test.txt");
-        writeFile("C:/Users/Alex Kopnin/Desktop/lib/test1.txt");
+        writeFile("C:/Users/Alex Kopnin/Desktop/lib/test1.txt", "text");
     }
 
     private static void readFile(String path) {
@@ -33,15 +33,14 @@ public class ReadWriteFile {
         }
     }
 
-    private static void writeFile(String path) {
+    private static void writeFile(String path, String content) {
         FileWriter writer = null;
         BufferedWriter bufferedWriter = null;
         try {
             writer = new FileWriter(path, true);
             bufferedWriter = new BufferedWriter(writer);
 
-            bufferedWriter.append("\n");
-            bufferedWriter.append("TTTT");
+            bufferedWriter.append(content);
         } catch (IOException e) {
             System.err.println("Can't write to file");
         } finally {
