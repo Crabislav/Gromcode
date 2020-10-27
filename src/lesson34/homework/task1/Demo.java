95f0c910aa34445499deb7edad830b7a528e8d19
Crabislav <alex.kopnin2@gmail.com>
2020-10-28 00:05:53 +0200

    add try/catch block

--- src/lesson34/homework/task1/Demo.java
+++ src/lesson34/homework/task1/Demo.java
@@ -1,10 +1,15 @@
 package lesson34.homework.task1;
 
 public class Demo {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String oldFilePath = "C:/Users/Alex Kopnin/Desktop/lib/test.txt";
         String newFilePath = "C:/Users/Alex Kopnin/Desktop/lib/newTest.txt";
 
-        Solution.transferFileContent(oldFilePath, newFilePath);
+        try {
+
+            Solution.transferFileContent(oldFilePath, newFilePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
