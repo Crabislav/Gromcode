3c543d01d5e72b948cd44c3669d17fe7428b37de
Crabislav <alex.kopnin2@gmail.com>
2020-11-05 23:16:57 +0200

    add hashCode and equals methods

--- src/lesson36/model/User.java
+++ src/lesson36/model/User.java
@@ -53,6 +53,30 @@ public class User extends Entity {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (userName != null ? !userName.equals(user.userName) : user.userName != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        if (country != null ? !country.equals(user.country) : user.country != null) return false;
+        return userType == user.userType;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (userType != null ? userType.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return id + ", " +
                 userName + ", " +
