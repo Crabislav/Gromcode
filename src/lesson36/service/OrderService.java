eee9a68c27145021fd1506608c475068fd99bc33
Crabislav <alex.kopnin2@gmail.com>
2020-11-05 23:57:30 +0200

    add dependencies, add constructor

--- src/lesson36/service/OrderService.java
+++ src/lesson36/service/OrderService.java
@@ -1,11 +1,19 @@
 package lesson36.service;
 
 import lesson36.repository.OrderRepository;
+import lesson36.repository.RoomRepository;
+import lesson36.repository.UserRepository;
 
 import java.util.Date;
 
 public class OrderService {
     private OrderRepository orderRepository = new OrderRepository();
+    private RoomRepository roomRepository = new RoomRepository();
+    private UserRepository userRepository = new UserRepository();
+
+    public OrderService() throws IOException {
+    }
+
 
     //TODO: finish
     public void bookRoom(long roomId, long userId, Date dateFrom, Date dateTo) {
