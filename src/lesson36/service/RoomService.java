6cf6f4968f9edb02d0a29557d6cd55f2a01864f6
Crabislav <alex.kopnin2@gmail.com>
2020-11-06 00:00:39 +0200

    add constructor, add dependencies

--- src/lesson36/service/RoomService.java
+++ src/lesson36/service/RoomService.java
@@ -2,12 +2,20 @@ package lesson36.service;
 
 import lesson36.model.Filter;
 import lesson36.model.Room;
+import lesson36.repository.OrderRepository;
+import lesson36.repository.RoomRepository;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class RoomService {
+    private RoomRepository roomRepository = new RoomRepository();
+    private OrderRepository orderRepository = new OrderRepository();
+
+    public RoomService() throws IOException {
+    }
 
     //TODO: finish
     public List<Room> findRooms(Filter filter) {
