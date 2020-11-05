18f44bbd2aff2dfd7d42eddf22ab970eb5d458ef
Crabislav <alex.kopnin2@gmail.com>
2020-11-05 23:58:31 +0200

    implement bookRoom, cancelReservation methods

--- src/lesson36/service/OrderService.java
+++ src/lesson36/service/OrderService.java
@@ -15,14 +15,83 @@ public class OrderService {
     }
 
 
-    //TODO: finish
-    public void bookRoom(long roomId, long userId, Date dateFrom, Date dateTo) {
-        orderRepository.bookRoom(roomId, userId, dateFrom, dateTo);
+    public void bookRoom(long roomId, long userId, Date dateFrom, Date dateTo) throws Exception {
+        validateDate(dateFrom, dateTo);
+        ServiceUtils.validateId(roomId);
+        ServiceUtils.validateId(userId);
+
+        //check up room existence
+        Room room = roomRepository.findObjById(roomId);
+        if (room == null) {
+            throw new BadRequestException("Room (id=" + roomId + ") wasn't found");
+        }
+
+        //change room's data field
+        Date roomDateAvailableFrom = room.getDateAvailableFrom();
+        //when room is not available
+        if (!roomDateAvailableFrom.equals(dateFrom) || roomDateAvailableFrom.after(dateFrom)) {
+            throw new BadRequestException(
+                    "bookRoom: Room(id=" + room.getId() +
+                            "dateAvailableFrom=" + room.getDateAvailableFrom() +
+                            "hotel id=" + room.getHotel().getId() +
+                            ")" +
+                            "is not available now");
+        }
+        //when room is available
+        room.setDateAvailableFrom(dateTo);
+
+        //check up user existence
+        User user = userRepository.findObjById(userId);
+        if (user == null) {
+            throw new BadRequestException("User with id=" + userId + ") wasn't found");
+        }
+
+        //create a new order
+        Order order = new Order(user, room, dateFrom, dateTo, room.getPrice());
+
+        //save order to repository
+        orderRepository.save(order);
     }
 
-    //TODO: finish
-    public void cancelReservation(long roomId, long userId) {
-        orderRepository.cancelReservation(roomId, userId);
+    public void cancelReservation(long roomId, long userId) throws Exception {
+        ServiceUtils.validateId(roomId);
+        ServiceUtils.validateId(userId);
+
+        //check up room existence
+        Room room = roomRepository.findObjById(roomId);
+        if (room == null) {
+            throw new BadRequestException("Room (id=" + roomId + ") wasn't found");
+        }
+
+        //if room exists
+        //check up user existence
+        User user = userRepository.findObjById(userId);
+        if (user == null) {
+            throw new BadRequestException("User with id=" + userId + ") wasn't found");
+        }
+
+        //find an order
+        for (Order order : orderRepository.getAllObjects()) {
+            if (order.getRoom().getId() == roomId && order.getUser().getId() == userId) {
+                //cancel reservation
+                orderRepository.remove(order);
+            }
+        }
+
+        //set room's dateAvailableFrom to new Date()
+        room.setDateAvailableFrom(new Date());
+    }
+
+    private void validateDate(Date dateFrom, Date dateTo) throws IllegalArgumentException {
+        String methodName = "validateDate";
+
+        if (dateFrom == null || dateTo == null) {
+            throw new IllegalArgumentException(methodName + ": date can't be null");
+        }
+
+        if (dateFrom.after(dateTo)) {
+            throw new IllegalArgumentException(methodName + ": dateFrom can't be after dateTo");
+        }
     }
 
 }
