73dba2db1f88ec8dd969886b6d0cc956ad59cf32
Crabislav <alex.kopnin2@gmail.com>
2020-11-06 00:02:09 +0200

    implement deleteRoom method

--- src/lesson36/service/RoomService.java
+++ src/lesson36/service/RoomService.java
@@ -1,13 +1,14 @@
 package lesson36.service;
 
+import lesson36.exceptions.BadRequestException;
 import lesson36.model.Filter;
+import lesson36.model.Order;
 import lesson36.model.Room;
 import lesson36.repository.OrderRepository;
 import lesson36.repository.RoomRepository;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class RoomService {
@@ -22,27 +23,58 @@ public class RoomService {
         return new ArrayList<>();
     }
 
-    //TODO: finish
-    public void bookRoom(long roomId, long userId, Date dateFrom, Date dateTo) {
-
-    }
-
-    //TODO: finish
-    public void cancelReservation(long roomId, long userId) {
-
-    }
-
-
-    //TODO: finish
     //only admins
     public void addRoom(Room room) throws Exception {
         validateRoom(room);
         roomRepository.save(room);
     }
 
-    //TODO: finish
     //only admins
-    public void deleteRoom(long RoomId) {
+    public void deleteRoom(long roomId) throws Exception {
+        //validate input
+        ServiceUtils.validateId(roomId);
 
+        String methodName = "deleteRoom";
+
+        //find a room by id
+        Room room = roomRepository.findObjById(roomId);
+        if (room == null) {
+            throw new BadRequestException(methodName + ": Room (id=" + roomId + ") wasn't found");
+        }
+
+        //delete room:
+        //if room is used at order - throw new Exception
+        for (Order order : orderRepository.getAllObjects()) {
+            if (order.getRoom().equals(room)) {
+                throw new BadRequestException(methodName + ": can't delete already booked room");
+            }
+        }
+
+        //if room isn't used ar order - delete it
+        roomRepository.remove(room);
+    }
+
+    private void validateRoom(Room room) throws BadRequestException {
+        String methodName = "validateRoom";
+        String msg = "can't be null";
+        if (room == null) {
+            throw new BadRequestException(methodName + ": Input" + msg);
+        }
+
+        if (room.getHotel() == null) {
+            throw new BadRequestException(methodName + ": room's hotel" + msg);
+        }
+
+        if (room.getDateAvailableFrom() == null) {
+            throw new BadRequestException(methodName + ": room's dateAvailableFrom" + msg);
+        }
+
+        if (room.getBreakfastIncluded() == null) {
+            throw new BadRequestException(methodName + ": room's breakfastIncluded" + msg);
+        }
+
+        if (room.getPetsAllowed() == null) {
+            throw new BadRequestException(methodName + ": room's petsAllowed" + msg);
+        }
     }
 }
