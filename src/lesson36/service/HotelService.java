3286e612ec6477afff42b2b0fb75efeb6b9100f8
Crabislav <alex.kopnin2@gmail.com>
2020-11-05 23:56:05 +0200

    implement findHotelByName, findHotelByCity, addHotel, deleteHotel methods; add constructor

--- src/lesson36/service/HotelService.java
+++ src/lesson36/service/HotelService.java
@@ -1,27 +1,96 @@
 package lesson36.service;
 
+import lesson36.exceptions.BadRequestException;
 import lesson36.model.Hotel;
+import lesson36.repository.HotelRepository;
+
+import java.io.IOException;
+
 
 public class HotelService {
+    private HotelRepository hotelRepository = new HotelRepository();
 
-    //TODO: finish
-    public Hotel findHotelByName(String name) {
-        return null;
+    public HotelService() throws IOException {
     }
 
-    //TODO: finish
-    public Hotel findHotelByCity(String city) {
-        return null;
+    public Hotel findHotelByName(String name) throws Exception {
+        String methodName = "findHotelByName";
+
+        //input validation
+        if (name == null || name.isBlank()) {
+            throw new BadRequestException(methodName + ": input can't be null");
+        }
+
+        //find a hotel by name
+        for (Hotel hotel : hotelRepository.getAllObjects()) {
+            if (hotel.getName().equals(name)) {
+                return hotel;
+            }
+        }
+
+        throw new BadRequestException(methodName + ": Hotel was not found");
     }
 
-    //only admins
-    //TODO: finish
-    public void addHotel(Hotel hotel) {
+    public Hotel findHotelByCity(String city) throws Exception {
+        String methodName = "findHotelByCity";
+
+        if (city == null) {
+            throw new BadRequestException(methodName + ": input can't be null");
+        }
+
+        for (Hotel hotel : hotelRepository.getAllObjects()) {
+            if (hotel.getCity().equals(city)) {
+                return hotel;
+            }
+        }
+
+        throw new BadRequestException(methodName + ": hotel was not found");
     }
 
-    //only admins
-    //TODO: finish
-    public void deleteHotel(long hotelId) {
+    public void addHotel(Hotel hotel) throws Exception {
+        validateHotel(hotel);
+        hotelRepository.save(hotel);
+    }
 
+    public void deleteHotel(long hotelId) throws Exception {
+        String methodName = "deleteHotel";
+
+        ServiceUtils.validateId(hotelId);
+
+        //find a hotel
+        for (Hotel hotel : hotelRepository.getAllObjects()) {
+            if (hotel.getId() == hotelId) {
+                //delete a hotel from repository
+                hotelRepository.remove(hotel);
+            }
+        }
+
+        //if there is no such hotel - throw Exception
+        throw new BadRequestException(methodName + ": hotel was not found");
+    }
+
+    private void validateHotel(Hotel hotel) throws BadRequestException {
+        String methodName = "validateHotel";
+        String msg = "field can't be null";
+
+        if (hotel == null) {
+            throw new BadRequestException(methodName + ": Input " + msg);
+        }
+
+        if (hotel.getCity() == null) {
+            throw new BadRequestException(methodName + ": hotel's city " + msg);
+        }
+
+        if (hotel.getName() == null) {
+            throw new BadRequestException(methodName + ": hotel's name " + msg);
+        }
+
+        if (hotel.getCountry() == null) {
+            throw new BadRequestException(methodName + ": hotel's country " + msg);
+        }
+
+        if (hotel.getStreet() == null) {
+            throw new BadRequestException(methodName + ": hotel's street " + msg);
+        }
     }
 }
