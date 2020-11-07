94233b76544edcf948eae37cbd59a200fc5cce82
Crabislav <alex.kopnin2@gmail.com>
2020-11-08 01:30:33 +0200

    add constructor, change methods' throwing exceptions

--- src/lesson36/controller/HotelController.java
+++ src/lesson36/controller/HotelController.java
@@ -1,37 +1,41 @@
 package lesson36.controller;
 
-import lesson36.exceptions.AuthorizationException;
-import lesson36.exceptions.NotEnoughRightsException;
 import lesson36.model.Hotel;
 import lesson36.service.HotelService;
+
+import java.io.IOException;
 
 public class HotelController extends Controller {
     private HotelService hotelService = new HotelService();
 
+    public HotelController() throws IOException {
+    }
+
+
     //TODO: test
-    public Hotel findHotelByName(String name) throws AuthorizationException {
-        checkIsUserAuthorized();
+    public Hotel findHotelByName(String name) throws Exception {
+        checkIsUserAuthorized("findHotelByName");
         return hotelService.findHotelByName(name);
     }
 
     //TODO: test
-    public Hotel findHotelByCity(String city) throws NotEnoughRightsException {
-        checkUserRights();
+    public Hotel findHotelByCity(String city) throws Exception {
+        checkIsUserAuthorized("findHotelByCity");
         return hotelService.findHotelByCity(city);
     }
 
     //only admins
     //TODO: test
-    public void addHotel(Hotel hotel) throws NotEnoughRightsException, AuthorizationException {
-        checkIsUserAuthorized();
+    public void addHotel(Hotel hotel) throws Exception {
+        checkIsUserAuthorized("addHotel");
         checkUserRights();
         hotelService.addHotel(hotel);
     }
 
     //only admins
     //TODO: test
-    public void deleteHotel(long hotelId) throws NotEnoughRightsException, AuthorizationException {
-        checkIsUserAuthorized();
+    public void deleteHotel(long hotelId) throws Exception {
+        checkIsUserAuthorized("deleteHotel");
         checkUserRights();
         hotelService.deleteHotel(hotelId);
     }
