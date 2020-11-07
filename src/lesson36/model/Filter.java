a7f146bf0fc3534ba5f4495fd273a76bda05b927
Crabislav <alex.kopnin2@gmail.com>
2020-11-08 01:31:54 +0200

    add fields' getters

--- src/lesson36/model/Filter.java
+++ src/lesson36/model/Filter.java
@@ -28,4 +28,40 @@ public class Filter {
         this.city = city;
         this.street = street;
     }
+
+    public Integer getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Boolean getBreakfastIncluded() {
+        return breakfastIncluded;
+    }
+
+    public Boolean getPetsAllowed() {
+        return petsAllowed;
+    }
+
+    public Date getDateAvailableFrom() {
+        return dateAvailableFrom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
 }
