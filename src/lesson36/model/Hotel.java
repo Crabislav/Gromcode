d4394170aea33c5420e19bcd7858a015f916b221
Crabislav <alex.kopnin2@gmail.com>
2020-11-08 01:32:26 +0200

    override hashCode and equals methods

--- src/lesson36/model/Hotel.java
+++ src/lesson36/model/Hotel.java
@@ -52,6 +52,30 @@ public class Hotel extends Entity {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Hotel hotel = (Hotel) o;
+
+        if (!id.equals(hotel.id)) return false;
+        if (!name.equals(hotel.name)) return false;
+        if (!country.equals(hotel.country)) return false;
+        if (!city.equals(hotel.city)) return false;
+        return street.equals(hotel.street);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + country.hashCode();
+        result = 31 * result + city.hashCode();
+        result = 31 * result + street.hashCode();
+        return result;
+    }
+
+    @Override
     public String toString() {
         return id.toString();
     }
