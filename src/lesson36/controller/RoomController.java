d704f90254d1fedbde2bcd3de445bb169e9bc1b0
Crabislav <alex.kopnin2@gmail.com>
2020-11-08 19:59:44 +0200

    code refactor

--- src/lesson36/controller/RoomController.java
+++ src/lesson36/controller/RoomController.java
@@ -1,34 +1,36 @@
 package lesson36.controller;
 
-import lesson36.exceptions.AuthorizationException;
-import lesson36.exceptions.NotEnoughRightsException;
-import lesson36.model.Filter;
+import lesson36.model.filter.Filter;
 import lesson36.model.Room;
 import lesson36.service.RoomService;
 
+import java.io.IOException;
 import java.util.List;
 
 public class RoomController extends Controller {
     private RoomService roomService = new RoomService();
 
+    public RoomController() throws IOException {
+    }
+
     //TODO: test
-    public List<Room> findRooms(Filter filter) throws NotEnoughRightsException {
-        checkUserRights();
+    public List<Room> findRooms(Filter filter) throws Exception {
+        checkIsUserAuthorized("findRooms");
         return roomService.findRooms(filter);
     }
 
     //TODO: test
-    //only admins
-    public void addRoom(Room room) throws NotEnoughRightsException, AuthorizationException {
-        checkIsUserAuthorized();
+    /**only admins*/
+    public void addRoom(Room room) throws Exception {
+        checkIsUserAuthorized("addRoom");
         checkUserRights();
         roomService.addRoom(room);
     }
 
     //TODO: test
-    //only admins
-    public void deleteRoom(long roomId) throws NotEnoughRightsException, AuthorizationException {
-        checkIsUserAuthorized();
+    /**only admins*/
+    public void deleteRoom(long roomId) throws Exception {
+        checkIsUserAuthorized("deleteRoom");
         checkUserRights();
         roomService.deleteRoom(roomId);
     }
