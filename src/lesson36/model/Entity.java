759ef6af0ac9e7d2b1e2042023b2e770723dd6a3
Crabislav <alex.kopnin2@gmail.com>
2020-11-08 20:02:20 +0200

    add equals and hashCode methods

--- src/lesson36/model/Entity.java
+++ src/lesson36/model/Entity.java
@@ -4,4 +4,8 @@ public abstract class Entity {
     public abstract Long getId();
 
     public abstract void setId(Long id);
+
+    public abstract boolean equals(Object o);
+
+    public abstract int hashCode();
 }
