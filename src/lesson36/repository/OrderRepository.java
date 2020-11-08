9aeb2eb6a3be99acbd64b15a8e1beebde34675aa
Crabislav <alex.kopnin2@gmail.com>
2020-11-09 00:14:26 +0200

    code refactor, implement getMappedObject

--- src/lesson36/repository/OrderRepository.java
+++ src/lesson36/repository/OrderRepository.java
@@ -1,27 +1,39 @@
 package lesson36.repository;
 
 import lesson36.model.Order;
+import lesson36.model.Room;
+import lesson36.model.User;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class OrderRepository extends Repository<Order> {
+    private UserRepository userRepository = new UserRepository();
+    private RoomRepository roomRepository = new RoomRepository();
+    private HotelRepository hotelRepository = new HotelRepository();
 
     public OrderRepository() throws IOException {
         setPath("C:/Users/Alex Kopnin/Desktop/lesson36/OrderDb.txt");
         RepositoryUtils.createFileIfNotExists(getPath());
     }
 
-    //TODO: finish
     @Override
-    Order getMappedObject(String[] objValues) {
+    Order getMappedObject(String[] objValues) throws Exception {
         Long id = Long.parseLong(objValues[0]);
-//        User user = objValues[1];
-//        Room room;
-//        Date dateFrom;
-//        Date dateTo;
-//        Double moneyPaid;
-//        return new Order(id, user, room, dateFrom, dateTo, moneyPaid);
-        return null;
+
+        User user = userRepository.findObjById(Long.parseLong(objValues[1]));
+        Room room = roomRepository.findObjById(Long.parseLong(objValues[2]));
+
+        Date dateFrom = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", new Locale("us"))
+                .parse(objValues[3]);
+        Date dateTo = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", new Locale("us"))
+                .parse(objValues[4]);
+
+        Double moneyPaid = Double.parseDouble(objValues[5]);
+
+        return new Order(id, user, room, dateFrom, dateTo, moneyPaid);
     }
 
     @Override
